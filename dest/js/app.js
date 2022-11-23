ce80203dc5796d46ba1a897c4f18bb3a8aca0587
小林梨沙 <risap1275@gmail.com>
2022-11-23 22:43:51 +0900

    first commit

--- dest/js/app.js
+++ dest/js/app.js
@@ -37,8 +37,10 @@ const accordion = document.querySelectorAll(".js-accordion");
 
 function toggle() {
     const content = this.nextElementSibling;
+    const children = this.firstElementChild;
     this.classList.toggle("is-active");
     content.classList.toggle("is-open");
+    children.classList.toggle("is-open");
 }
 
 for (let i = 0; i < accordion.length; i++) {
