debe5318bfb713f7ba99626ad59efc76fe190340
小林梨沙 <risap1275@gmail.com>
2022-12-07 23:58:20 +0900

    gulpファイル変更

--- dest/js/app.js
+++ dest/js/app.js
@@ -1,38 +1,23 @@
 /*
-ハンバーガーメニュー
+ハンバーガーメニュー(ヘッダー)
 */
-const menu_area = document.querySelector('.header-button__inner');
-const menu = document.querySelector('.header__nav');
+const menuButton = document.querySelector('.js-button__inner');
+const menu = document.querySelector('.js-menu__nav');
+const menuList = document.querySelector('.js-menu__list');
 
-menu_area.addEventListener('click', function(){
-    menu_area.classList.toggle('active');
+menuButton.addEventListener('click', function(){
+    menuButton.classList.toggle('active');
     menu.classList.toggle('active');
 });
+menuList.addEventListener('click', function(){
+    menuButton.classList.remove('active');
+    menu.classList.remove('active');
+});
 
+/*
+アコーディオン(フッター)
+*/
 
-// メインビジュアル画像切り替え
-// const img = ["images/top/keyv-pc_01.jpg","images/top/keyv-pc_02.jpg", "images/top/keyv-pc_03.jpg"];
-// let count = -1;
-
-// picChange(); // 関数を実行
-
-// function picChange() {
-
-// count++;
-
-// // カウントが最大になれば配列を初期値に戻すため「0」を指定する
-// if (count == img.length) count = 0;
-
-// // 画像選択
-// document.getElementById("pic").src = img[count];
-
-// // 5秒ごとに実行
-// setTimeout("picChange()", 5000);
-
-// }
-
-
-// フッターのアコーディオン
 const accordion = document.querySelectorAll(".js-accordion");
 
 function toggle() {
--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,38 @@
 // gulpプラグインの読み込み
 const gulp = require("gulp");
-// Sassをコンパイルするプラグインの読み込み
+
+
+/*
+sass→cssコンパイルのタスク gulp sass
+*/
 const sass = require("gulp-sass")(require("sass"));
 
-// style.scssをタスクを作成する
-gulp.task("default", () => {
-  // style.scssファイルを取得
+gulp.task('sass', () => {
   return (
     gulp
       .src("scss/*.scss")
-      // Sassのコンパイルを実行
       .pipe(sass())
-      // cssフォルダー以下に保存
       .pipe(gulp.dest("dest/css"))
   );
 });
 
-// var gulp = require("gulp");
-// var rename = require("gulp-rename");
-// var ejs = require("gulp-ejs");
-// var replace = require("gulp-replace");
+/*
+画像圧縮のタスク gulp imagemin
+*/
+const imagemin = require('gulp-imagemin');
 
-// gulp.task("ejs", (done) => {
-// 	gulp
-// 		.src(["ejs/**/*.ejs", "!" + "ejs/**/_*.ejs"])
-// 		.pipe(ejs({}, {}, {ext:'.html'}))
-// 		.pipe(rename({ extname: ".html" }))
-//         .pipe(replace(/[\s\S]*?(<!DOCTYPE)/, "$1"))
-// 		.pipe(gulp.dest("./"));
-// 	done();
-// });
+gulp.task('imagemin', () => {
+  return (
+    gulp.src('images/**/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('dest/images/'))
+  );
+});
 
-
-// // const gulp = require('gulp');
-// const imagemin = require('gulp-imagemin');
- 
-// exports.default = () => (
-// 	gulp.src('images/**/*')
-// 		.pipe(imagemin())
-// 		.pipe(gulp.dest('dest/images/'))
-// );
-
-
-
-// gulp htmlbeautifyを実行
-var htmlbeautify = require("gulp-html-beautify");
+/*
+ejsをhtmlにコンパイル後にインデントを整えるタスク gulp htmlbeautify
+*/
+const htmlbeautify = require("gulp-html-beautify");
 
 gulp.task('htmlbeautify', function(done) {
   var options = {
